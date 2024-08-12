3778c3e6facca5b6ccc8cc4603082b07fea77246
minwater96 <rlaalstn2568@naver.com>
2024-08-12 15:33:49 +0900

    follow 기능 구현을 위한 모델수정

--- accounts/models.py
+++ accounts/models.py
@@ -10,3 +10,4 @@ class User(AbstractUser):
         crop=['middle', 'center'],
         upload_to='profile'
     )
+    followings = models.ManyToManyField('self', related_name='followers', symmetrical=False)
